d68a1124ff954162b0d66118e3b39bc319872173
wzrdk3lly <dkmk1149@gmail.com>
2023-02-04 11:40:00 -0500

    FIX form

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,11 +3,14 @@ import meme from "../assets/memeimg.png";
 export default function MainSection() {
   return (
     <main className="main--content">
-      <div className="div--input">
-        <input type="text" />
-        <input type="text" />
-      </div>
-      <button className="btn--newImg">Get a new meme image 🖼 </button>
+      <form>
+        <div className="div--input">
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <button className="btn--newImg">Get a new meme image 🖼 </button>
+      </form>
+
       <img className="img--meme" src={meme} alt="" />
     </main>
   );
