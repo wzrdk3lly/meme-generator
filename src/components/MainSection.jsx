c9b81096ee4399be61faec188050e2585cda7d7b
wzrdk3lly <dkmk1149@gmail.com>
2023-02-06 11:46:40 -0500

    ADD State Functionality to update UI on event change

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,17 +1,28 @@
 import meme from "../assets/memeimg.png";
+import memeData from "../memesData.jsx";
+import React from "react";
 
 export default function MainSection() {
+  let [imageUrl, setImageUrl] = React.useState("");
+
+  function handleOnClick() {
+    let arrayOfImages = memeData.data.memes;
+    let randomImageObject =
+      arrayOfImages[Math.floor(Math.random() * arrayOfImages.length)];
+
+    setImageUrl(randomImageObject.url);
+  }
   return (
     <main className="main--content">
-      <form>
-        <div className="div--input">
-          <input type="text" />
-          <input type="text" />
-        </div>
-        <button className="btn--newImg">Get a new meme image 🖼 </button>
-      </form>
-
-      <img className="img--meme" src={meme} alt="" />
+      <div className="div--input">
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <button className={"btn--newImg"} onClick={handleOnClick}>
+        Get a new meme image 🖼{" "}
+      </button>
+      {/* conditional rendering to create img incase there is no image url */}
+      {imageUrl && <img className="img--meme" src={imageUrl} />}
     </main>
   );
 }
